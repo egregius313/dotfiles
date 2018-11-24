5b46c3a087a9767d8374ed226b18f04a273370be
Ed Minnix <egregius313@gmail.com>
2018-11-23 22:19:33 -0500

    Updated aliases

--- aliases.sh
+++ aliases.sh
@@ -1,8 +1,28 @@
 #!/bin/bash
 
+set_home () {
+    if [ -z $2 ]
+    then
+        ln -s $DOTFILES/$1 ~/$1;
+    else
+        ln -s $DOTFILES/$1 ~/$2;
+    fi
+}
+
 # Configure git
-ln -s $DOTFILES/gitignore ~/.gitignore
-ln -s $DOTFILES/gitconfig ~/.gitconfig
+set_home gitignore .gitignore
+set_home gitconfig .gitconfig
+
+# Configure shell files
+
+set_home .bashrc
+set_home .zshrc
+set_home .profile
+
+# tmux
+
+set_home .tmux.conf
+
 
 # Configure .npmrc 
 
